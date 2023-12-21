2126fa56fc96f76f55a24399a99cf860a9a33c1b
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-21 08:30:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,5 @@ async def start(user, message):
 
 #==========================================================
 
-Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{PORT}", shell=True)
 Popen("python3 -m utils.delete", shell=True)
 User.run()
